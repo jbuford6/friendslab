710ba8d219584ba3686e6495e5706afa0d89006c
InnovateBham_5 <InnovateBham_5@UABiLab-5s-iMac.local>
2017-06-16 17:08:08 -0500

    finished friends lab

---
+++ friends.js
@@ -0,0 +1,28 @@
+var friends = ["Bill Gates","Lil Wayne","Kobe Bryant","Jesus","Taylor Swift"];
+    
+
+for (var i = 0; i < friends.length; i++ ) {
+      console.log(friends[i]+ ":");
+    for (var j = 99; j > 0; j--) {
+        
+        if (j>2){
+            console.log(j + " lines of code in the file, lines of code; " + (friends)[i] + " strikes one out, clears it all out," + (j-1) + "lines of code in the file.")
+        } else if (j == 2) {
+            console.log(j + " line of code in the file, line of code; " + (friends)[i] + " strikes one out, clears it all out, no more lines of code in the file.")
+        } else {
+            console.log(j + " line of code in the file, line of code; " + (friends)[i] + " strikes one out, clears it all out, no more lines of code in the file.")
+
+
+        }
+}
+}
+   
+
+    
+    
+    // console.log(friends[i]+ ".");
+    // console.log(j + " lines of code in the file, lines of code; strikes one out, clears it all out,");
+
+    
+
+
